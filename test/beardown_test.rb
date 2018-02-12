9cdd90e2494869522fd490a1fe69ffaf6b13c4c5
wuusn <wuusin@gmail.com>
2018-02-12 19:08:21 +0800

    basic test

--- test/beardown_test.rb
+++ test/beardown_test.rb
@@ -8,4 +8,39 @@ class BeardownTest < Minitest::Test
   def test_it_does_something_useful
     assert true
   end
+
+  def parse(doc)
+    Beardown.new(doc).to_html
+  end
+
+  def test_headings
+    assert_equal parse("# Beardown Test"), "<h1>Beardown Test</h1>"
+    assert_equal parse("## Heading 2"), "<h2>Heading 2</h2>"
+    assert_equal parse("### Heading 3"), "<h3>Heading 3</h3>"
+  end
+
+  def test_bold
+  end
+
+  def test_italic
+  end
+
+  def test_underline
+  end
+
+  def strike
+  end
+
+  def test_link
+  end
+
+  def test_list
+  end
+
+  def test_list_ordered
+  end
+
+  def test_quote
+  end
+
 end
